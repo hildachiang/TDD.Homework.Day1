87944a5ef6b2e002fe0ddcfddf532ee557c46d9a
WantingChiang <WantingChiang@WANTING-PC>
2017-01-10 02:24:21 +0800

    建立測試專案

---
+++ TDD.Homework.Day1.ContextTests/GroupSumServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDD.Homework.Day1.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD.Homework.Day1.Context.Tests
+{
+    [TestClass()]
+    public class GroupSumServiceTests
+    {
+        [TestMethod()]
+        public void GetSummaryTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
